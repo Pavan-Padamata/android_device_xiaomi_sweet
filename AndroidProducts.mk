7b71af95613f71cdd312631c96a0cee3641b5e1e
PavanPadamata <its4tech54@gmail.com>
2023-05-06 08:40:05 +0000

    sweet:Zephyrus Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/zep_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sweet-user \
-    lineage_sweet-userdebug \
-    lineage_sweet-eng
+    zep_sweet-user \
+    zep_sweet-userdebug \
+    zep_sweet-eng
